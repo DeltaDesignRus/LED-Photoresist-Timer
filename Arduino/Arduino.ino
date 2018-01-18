35cb5c62edfa6c2e638949eb649844a543b5497b
DeltaDesignRus <DeltaDesignRus@gmail.com>
2018-01-18 13:12:24 +0500

    Добавлена возможность сохранять пресеты в EEPROM долгим нажатием на кнопки

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,93 +1,117 @@
 #include <LiquidCrystal.h>
-#include <Bounce.h>
+#include <OneButton.h>
+#include <EEPROM.h>
 
-int PinPres1 = 5; // кнопки
-int PinPres2 = 6;
-int PinRelay = 13; // реле
+OneButton PinPres1(6, false); // кнопка 1 (верхняя)
+OneButton PinPres2(5, false); // кнопка 2 (нижняя)
+
+int PinRelay = 13; //выход затвор транзистора
 int PinCLK = 2; // энкодер
 int PinDT = 3;
-int PinSW = 4;
+int PinSW = 4; // кнопка энкодера
 long Preset0 = 0;  // начальное значение (сек.)
-long Preset1 = 20;  // предустановка времени №1 (сек.)
-long Preset2 = 140;  // предустановка времени №2 (сек.)
+long Preset1 = 20;  // предустановка времени №1 (сек.) используемая по умолчанию
+long Preset2 = 140;  // предустановка времени №2 (сек.) используемая по умолчанию
 int sec_step = 5; // шаг энкодера (сек.)
 
 unsigned long currentTime; // текущее время
 unsigned long loopTime; // время окончания
 unsigned long LimitTime = 0; // таймер
+unsigned long Sum1 = 0; //переменная вычисляемого значения контрольной суммы для пресета 1
+unsigned long Sum2 = 0; //переменная вычисляемого значения контрольной суммы для пресета 2
+unsigned long CheckSum1 = 0; //переменная считываемого из EEPROM значения контрольной суммы для пресета 1
+unsigned long CheckSum2 = 0; //переменная считываемого из EEPROM значения контрольной суммы для пресета 2
 volatile boolean turn;
 volatile boolean up;
+long DefSaveReadNum = 18527258;
 boolean running = false; // флаг запуска отсчета
-int awake1, awake2, awake3;
+int awake;
+
+// чтение из EEPROM
+unsigned long EEPROM_ulong_read(int addr) {
+  byte raw[4];
+  for (byte i = 0; i < 4; i++) raw[i] = EEPROM.read(addr + i);
+  unsigned long &num = (unsigned long&)raw;
+  return num;
+}
+
+// запись в EEPROM
+void EEPROM_ulong_write(int addr, unsigned long num) {
+  byte raw[4];
+  (unsigned long&)raw = num;
+  for (byte i = 0; i < 4; i++) EEPROM.write(addr + i, raw[i]);
+}
 
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7); // экран
-Bounce bouncer1 = Bounce(PinPres1, 5); // дребезг кнопок
-Bounce bouncer2 = Bounce(PinPres2, 5); 
-Bounce bouncer3 = Bounce(PinSW, 5); 
 
 void setup() {
 
-  Preset0 = Preset0*1000;
-  Preset1 = Preset1*1000;
-  Preset2 = Preset2*1000;
-  sec_step = sec_step*1000;
+  Serial.begin(9600);
+  Preset0 = Preset0 * 1000;
+  Preset1 = Preset1 * 1000;
+  Preset2 = Preset2 * 1000;
+  sec_step = sec_step * 1000;
   LimitTime = Preset0;
   pinMode(PinCLK, INPUT); // входы
-  pinMode(PinDT, INPUT);  
+  pinMode(PinDT, INPUT);
   pinMode(PinSW, INPUT);
-  pinMode(PinPres1, INPUT);
-  pinMode(PinPres2, INPUT);
   pinMode(PinRelay, OUTPUT);
   attachInterrupt (0, isr, CHANGE); // прерывание энкодера
   lcd.begin(16, 2);
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("  UV LED TIMER  ");
-  lcd.setCursor(0,1);
-  lcd.print("    ver. 1.0    ");
+  lcd.setCursor(0, 1);
+  lcd.print("    ver. 1.1    ");
   delay(1000);
   lcd.clear();
   currentTime = millis();
+  PinPres1.attachClick(PinPres1click);
+  PinPres1.attachLongPressStart(PinPres1longPressStart);
+  PinPres2.attachClick(PinPres2click);
+  PinPres2.attachLongPressStart(PinPres2longPressStart);
+
+    if (EEPROM_ulong_read(0) != DefSaveReadNum) { //проверка на первый запуск
+    EEPROM_ulong_write(0, DefSaveReadNum);
+  }
 }
 
 void loop() {
 
-  delay(200);
-
-  bouncer1.update(); // дребезг кнопок
-  bouncer2.update();
-  bouncer3.update();
- 
   // выбран режим отсчета
   /* ========================== */
-  if (running == true) {
+  if (running == true) { //Если флаг запуска отсчета True, то:
 
     // отсчет времени
     currentTime = millis();
     loopTime = currentTime + LimitTime; // когда закончить
-    while(currentTime < loopTime) { // сравниваем текущее время с временем окончания
+    while (currentTime < loopTime) { // сравниваем текущее время с временем окончания
 
-      digitalWrite(PinRelay, HIGH); // включаем
+      digitalWrite(PinRelay, HIGH); // включаем (подаем 5в на затвор транзистора подключая нагрузку через транзистор)
 
       // вывод на экран
-      lcd.setCursor(0,0);
-      lcd.print("   Working...   "); 
-      lcd.setCursor(4, 2);  
-      if (LimitTime/60/60/1000 < 10) { lcd.print("0"); }
-      lcd.print(LimitTime/60/60/1000);
+      lcd.setCursor(0, 0);
+      lcd.print("    EXPOSING    ");
+      lcd.setCursor(4, 2);
+      if (LimitTime / 60 / 60 / 1000 < 10) {
+        lcd.print("0");
+      }
+      lcd.print(LimitTime / 60 / 60 / 1000);
       lcd.print(":");
-      if (LimitTime/60/1000%60 < 10) { lcd.print("0"); }
-      lcd.print((LimitTime/60/1000)%60);
+      if (LimitTime / 60 / 1000 % 60 < 10) {
+        lcd.print("0");
+      }
+      lcd.print((LimitTime / 60 / 1000) % 60);
       lcd.print(":");
-      if (LimitTime/1000%60 < 10) { lcd.print("0"); }
-      lcd.print(LimitTime/1000%60);
+      if (LimitTime / 1000 % 60 < 10) {
+        lcd.print("0");
+      }
+      lcd.print(LimitTime / 1000 % 60);
       lcd.display();
 
-      // кнопка
-      bouncer3.update();
-      if (bouncer3.read() != awake3) {
- 
-        awake3 = bouncer3.read();
-        if (awake3 && running == true) {
+      // кнопка энкодера
+      if (digitalRead(PinSW) != awake) {
+        awake = digitalRead(PinSW);
+        if (awake && running == true) {
           running = false;
           digitalWrite(PinRelay, HIGH); // включаем
           LimitTime = Preset0;
@@ -99,7 +123,7 @@ void loop() {
       currentTime = millis(); // получаем новое время
     }
 
-    // окончание работы таймера 
+    // окончание работы таймера
     digitalWrite(PinRelay, LOW); // отключаем устройство
 
     lcd.setCursor(0, 0);
@@ -108,56 +132,43 @@ void loop() {
     lcd.print("     FINISH     ");
     running = false;
     delay (3000);
-    //lcd.begin(16, 2);
 
-  // выбран режим клавиатуры
-  /* ========================== */
   } else {
 
-    digitalWrite(PinRelay, LOW); // отключаем устройство  
-    
+    digitalWrite(PinRelay, LOW); // отключаем устройство
+
     if (LimitTime > 86400000) LimitTime = Preset0; // отбрасываем некорректные значения
-    
+
     // вывод на экран
-    lcd.setCursor(0,0);
-    lcd.print("   Set timer:   "); 
-    lcd.setCursor(4, 2);  
-    if (LimitTime/60/60/1000 < 10) { lcd.print("0"); }
-    lcd.print(LimitTime/60/60/1000);
+    lcd.setCursor(0, 0);
+    lcd.print("   Set timer:   ");
+    lcd.setCursor(4, 2);
+    if (LimitTime / 60 / 60 / 1000 < 10) {
+      lcd.print("0");
+    }
+    lcd.print(LimitTime / 60 / 60 / 1000);
     lcd.print(":");
-    if (LimitTime/60/1000%60 < 10) { lcd.print("0"); }
-    lcd.print((LimitTime/60/1000)%60);
+    if (LimitTime / 60 / 1000 % 60 < 10) {
+      lcd.print("0");
+    }
+    lcd.print((LimitTime / 60 / 1000) % 60);
     lcd.print(":");
-    if (LimitTime/1000%60 < 10) { lcd.print("0"); }
-    lcd.print(LimitTime/1000%60);
+    if (LimitTime / 1000 % 60 < 10) {
+      lcd.print("0");
+    }
+    lcd.print(LimitTime / 1000 % 60);
     lcd.display();
 
     // пресет №1
-    if (bouncer1.read() != awake1) {
-      awake1 = bouncer1.read();
-      if (awake1) {
-        lcd.setCursor(0,1);
-        lcd.print("    preset 1    ");
-        LimitTime = Preset1;
-        delay(1000);
-      }
-    }
-
+    PinPres1.tick();
     // пресет №2
-    if (bouncer2.read() != awake2) {
-      awake2 = bouncer2.read();
-      if (awake2) {
-        lcd.setCursor(0,1);
-        lcd.print("    preset 2    ");
-        LimitTime = Preset2;
-        delay(1000);
-      }
-    }
+    PinPres2.tick();
 
     // энкодер
-    static long enc_pos=0;
+    static long enc_pos = 0;
+
     if (digitalRead(PinSW) && enc_pos != 0) {
-      enc_pos=0;
+      enc_pos = 0;
     }
     if (turn) {
       if (up) {
@@ -166,13 +177,13 @@ void loop() {
         enc_pos++;
       }
       turn = false;
-      LimitTime += enc_pos*sec_step;
+      LimitTime += enc_pos * sec_step;
     }
 
-    // кнопка
-    if (bouncer3.read() != awake3) {
-      awake3 = bouncer3.read();
-      if (awake3 && LimitTime != 0) {
+    // кнопка энкодера
+    if (digitalRead(PinSW) != awake) {
+      awake = digitalRead(PinSW);
+      if (awake && LimitTime != 0) {
         running = true;
       }
     }
@@ -183,4 +194,56 @@ void loop() {
 void isr ()  {
   up = (digitalRead(PinCLK) == digitalRead(PinDT));
   turn = true;
+}
+
+void PinPres1click() {
+  lcd.setCursor(0, 0);
+  lcd.print("    Preset 1    ");
+
+  CheckSum1 = EEPROM_ulong_read(12); //чтение контрольной суммы пресета 1 из памяти
+  Sum1 = EEPROM_ulong_read(0) + EEPROM_ulong_read(4); //расчет контрольной суммы пресета 1
+
+  if (CheckSum1 != Sum1 || CheckSum1 == 0) { //сравнение прочитанной и рассчитаной контрольных сумм  пресета 1
+    LimitTime = Preset1; //Устанавливаем дефолтное значение пресета 1
+  } else {
+    LimitTime = EEPROM_ulong_read(4);  //читаем время пресета 1
+  }
+  delay(1000);
+}
+
+void PinPres1longPressStart() {
+  lcd.setCursor(0, 0);
+  lcd.print(" Preset 1: save ");
+  if (EEPROM_ulong_read(0) != DefSaveReadNum) { //проверка на первый запуск
+    EEPROM_ulong_write(0, DefSaveReadNum);
+  }
+  EEPROM_ulong_write(4, LimitTime); //запись в EEPROM времени пресета 1
+  EEPROM_ulong_write(12, EEPROM_ulong_read(0) + EEPROM_ulong_read(4)); //запись контрольной суммы CheckSum1
+  delay(1000);
+}
+
+void PinPres2click() {
+  lcd.setCursor(0, 0);
+  lcd.print("    Preset 2    ");
+
+  CheckSum2 = EEPROM_ulong_read(16); //чтение контрольной суммы пресета 2 из памяти
+  Sum2 = EEPROM_ulong_read(0) + EEPROM_ulong_read(8); //расчет контрольной суммы пресета 2
+
+  if (CheckSum2 != Sum2 || CheckSum2 == 0) { //сравнение прочитанной и рассчитаной контрольных сумм  пресета 2
+    LimitTime = Preset2; //Устанавливаем дефолтное значение пресета 2
+  } else {
+    LimitTime = EEPROM_ulong_read(8); //читаем время пресета 2
+  }
+  delay(1000);
+}
+
+void PinPres2longPressStart() {
+  lcd.setCursor(0, 0);
+  lcd.print(" Preset 2: save ");
+  if (EEPROM_ulong_read(0) != DefSaveReadNum) { //проверка на первый запуск
+    EEPROM_ulong_write(0, DefSaveReadNum);
+  }
+  EEPROM_ulong_write(8, LimitTime); //запись в EEPROM времени пресета 2
+  EEPROM_ulong_write(16, EEPROM_ulong_read(0) + EEPROM_ulong_read(8)); //запись контрольной суммы CheckSum2
+  delay(1000);
 }
